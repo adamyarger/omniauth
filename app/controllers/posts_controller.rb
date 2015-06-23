6371210f5ffe9a51d2297973bee3263912123bc8
adamyarger <yargeradam@gmail.com>
2015-06-22 19:38:50 -0500

    devise setup

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,32 @@
 class PostsController < ApplicationController
+
+	def index
+		@posts = Post.all
+	end
+
+	def show
+		@post = Post.find(params[:id])
+	end
+
+	def create
+		@post = current_user.posts.build(post_params)
+
+		if @post.save
+			flash[:success] = 'Post Success!'
+			redirect_to root_path
+		else
+			flash[:error] = 'Something went wrong'
+			render :new
+		end
+	end
+
+	def new
+		@post = Post.new
+	end
+
+	private
+
+		def post_params
+			params.require(:post).permit(:content)
+		end
 end
